52019b09c224ea621b857e92177a469ef76f0622
Roostars <isaacus95@gmail.com>
2023-06-28 18:16:45 -0700

    pantallas de app curso kotlin 28/06/2023

--- app/src/main/java/com/akagami/example2/androidexercise/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/akagami/example2/androidexercise/imccalculator/ImcCalculatorActivity.kt
@@ -2,9 +2,12 @@ package com.akagami.example2.androidexercise.imccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.akagami.example2.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var viewFemale:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
